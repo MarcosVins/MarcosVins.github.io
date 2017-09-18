40b54411443f5ee4e1bbfd66f518748b485be5d1
Marcos Vinicius <marcos.vsaints@gmail.com>
2017-09-18 14:54:53 -0300

    Add files via upload

---
+++ projetos/acafeteria/js/main.js
@@ -0,0 +1,6 @@
+$(".rslides_lojas").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+  pager: true,           // Boolean: Show pager, true or false
+});
